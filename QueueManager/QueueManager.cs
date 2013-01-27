6d1fb9a4b295d99f4167840b68f6e28f1f73cf5f
Kevin Brown <bass5098@gmail.com>
2013-01-27 10:14:55 -0500

    Updated QueueManager to deal with rules

--- QueueManager/QueueManager.cs
+++ QueueManager/QueueManager.cs
@@ -13,53 +13,242 @@ namespace QueueManager
         Dictionary<string, int> Timeouts = new Dictionary<string, int>();
 
         /// <summary>
-        /// A Dictionary that holds the lists of rules for each priority
+        /// A Dictionary that holds the lists of rules for each priority.
         /// </summary>
         Dictionary<RulePriority, List<Rule>> Rules = new Dictionary<RulePriority, List<Rule>>();
 
         /// <summary>
-        /// True if the Queue is waiting for a finish callback, False if not
+        /// True if the Queue is waiting for a finish callback, False if not.
         /// </summary>
         private bool IsRunning = false;
 
         /// <summary>
+        /// The class constructor for the QueueManager.
+        /// </summary>
+        public QueueManager()
+        {
+            // Initialize the rules dictionary
+
+            Rules[RulePriority.High] = new List<Rule>();
+            Rules[RulePriority.Medium] = new List<Rule>();
+            Rules[RulePriority.Low] = new List<Rule>();
+        }
+
+        /// <summary>
+        /// Adds the specified item to the specified queue.
+        /// </summary>
+        /// <param name="name">The name of the queue.</param>
+        /// <param name="item">The item to add to the queue.</param>
+        public void AddItem(string name, object item)
+        {
+            // Add the item to the specified queue
+
+            Queues[name].Enqueue(item);
+        }
+
+        /// <summary>
         /// Adds a new Queue to the dictionary that is initialized to accept anything.
         /// </summary>
-        /// <param name="name">The name of the Queue</param>
-        /// <param name="callback">The callback that will be called when the queue is executed</param>
-        /// <param name="timeout">The amount of time the QueueManager will wait before it continues</param>
+        /// <param name="name">The name of the Queue.</param>
+        /// <param name="callback">The callback that will be called when the queue is executed.</param>
+        /// <param name="timeout">The amount of time the QueueManager will wait before it continues.</param>
         public void AddQueue(string name, Action<object, Action> callback, int timeout)
         {
+            // Add the necessary data to the dictionaries
+
             Queues.Add(name, new Queue<object>());
             Callbacks.Add(name, callback);
             Timeouts.Add(name, timeout);
         }
 
         /// <summary>
-        /// Adds a rule which is used to determine the order of Queues
+        /// Adds a rule which is used to determine the order of Queues.
         /// </summary>
-        /// <param name="rule">The new rule</param>
+        /// <param name="rule">The new rule.</param>
         public void AddRule(Rule rule)
         {
+            // Add the rule to the specific list for its priority
+
             Rules[rule.Priority].Add(rule);
         }
 
         /// <summary>
-        /// If the queue is not waiting on a callback, executes the next available queue item
+        /// If the queue is not waiting on a callback, executes the next available queue item.
         /// </summary>
         public void Continue()
         {
+            // Don't continue if the queue is already processing an item
+
             if (!IsRunning)
             {
+                // Get the name of the next queue to be processed
 
+                string queueName = DetermineNextQueue();
+
+                // Don't continue if the queues are empty
+
+                if (queueName == null)
+                {
+                    return;
+                }
+
+                Queue<object> queue = Queues[queueName];
+
+                // Don't continue if the queue is empty
+
+                if (queue.Count == 0)
+                {
+                    return;
+                }
+
+                // Invoke the callback for the specific queue
+
+                Callbacks[queueName](queue.Dequeue(), EndCallback);
             }
         }
 
         /// <summary>
-        /// If the Queue is currently waiting on a callback, force the Queue to restart
+        /// Determines the next queue that will be called.
+        /// </summary>
+        /// <returns>
+        /// The name of the next Queue that will be called.
+        /// Returns null if all queues are empty.
+        /// Returns the first queue if all rules result in a tie.
+        /// </returns>
+        private string DetermineNextQueue()
+        {
+            // Return "null" only if all queues are empty
+
+            if (Empty())
+            {
+                return null;
+            }
+
+            // Check all priorities
+
+            foreach (KeyValuePair<RulePriority, List<Rule>> priority in Rules)
+            {
+                // Query for the next queue on the priority
+
+                string queueName = DetermineNextQueue(priority.Key);
+
+                // If the query doesn't return null, it found a queue
+
+                if (queueName != null)
+                {
+                    return queueName;
+                }
+            }
+
+            // In the event of a tie, return the name of the first queue.
+
+            return Queues.Keys.ElementAt(0);
+        }
+
+        /// <summary>
+        /// Determines the next queue that will be called,
+        /// </summary>
+        /// <param name="priority">The priority that will be used to determine the rules.</param>
+        /// <returns>Returns null if there is a rule tie.  Returns the queue name if possible.</returns>
+        private string DetermineNextQueue(RulePriority priority)
+        {
+            Dictionary<string, int> priorities = new Dictionary<string, int>();
+
+            // Run through all high priority rules first
+
+            foreach (Rule rule in Rules[priority])
+            {
+                if (rule.IsApplicable(Queues))
+                {
+                    // Initialize the priority of the queue as 0 if it does not already exist
+
+                    if (!priorities.ContainsKey(rule.QueueName))
+                    {
+                        priorities.Add(rule.QueueName, 0);
+                    }
+
+                    // Add the value of the rule to the queue
+
+                    priorities[rule.QueueName] += rule.Value;
+                }
+            }
+
+            // Get the queue with the highest priority
+
+            string highestQueue = null;
+
+            foreach (KeyValuePair<string, int> queue in priorities)
+            {
+                // Set the first queue
+
+                if (highestQueue == null)
+                {
+                    highestQueue = queue.Key;
+                }
+                else
+                {
+                    // Check for a tie
+
+                    if (queue.Value == priorities[highestQueue])
+                    {
+                        highestQueue = null;
+
+                        break;
+                    }
+
+                    // Check if the current queue has a higher value than the previous highest
+
+                    if (queue.Value > priorities[highestQueue])
+                    {
+                        highestQueue = queue.Key;
+                    }
+                }
+            }
+
+            // Check for a tie
+
+            if (highestQueue == null)
+            {
+                return null;
+            }
+            else
+            {
+                return highestQueue;
+            }
+        }
+
+        /// <summary>
+        /// Determine if all queues are empty.
+        /// </summary>
+        /// <returns>True if all queues are empty, false otherwise.</returns>
+        public bool Empty()
+        {
+            foreach (KeyValuePair<String, Queue<object>> queue in Queues)
+            {
+                if (queue.Value.Count > 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// The callback that should be called when a queue is done processing its item.
+        /// </summary>
+        private void EndCallback()
+        {
+            IsRunning = false;
+        }
+
+        /// <summary>
+        /// If the Queue is currently waiting on a callback, force the Queue to restart.
         /// </summary>
         public void Restart()
         {
+            // Tell the manager it is no longer processing the current item
+
             if (IsRunning == true)
             {
                 IsRunning = false;
