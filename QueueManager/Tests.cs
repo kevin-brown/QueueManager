1d3d7c957a58da291b0ddbb8343377f13f832f1f
Kevin Brown <bass5098@gmail.com>
2013-01-27 10:15:49 -0500

    Updated the basic tests, will move to a testing suite soon

--- QueueManager/Tests.cs
+++ QueueManager/Tests.cs
@@ -8,9 +8,46 @@ namespace QueueManager
 {
     class Tests
     {
+        class TestRule : Rule
+        {
+            public TestRule()
+            {
+                Priority = RulePriority.High;
+                Value = 1;
+                QueueName = "test";
+            }
+
+            public override bool IsApplicable(Dictionary<string, Queue<object>> queues)
+            {
+                return true;
+            }
+        }
+
         static void Main(string[] args)
         {
             QueueManager manager = new QueueManager();
+
+            // Initialize the queue manager with test data
+
+            manager.AddRule(new TestRule());
+            manager.AddQueue("test", TestCallback, 1000);
+            manager.AddItem("test", "test item");
+
+            // Test to make sure the callback works
+
+            manager.Continue();
+
+            Console.ReadLine();
+        }
+
+        static void TestCallback(object item, Action callback)
+        {
+            Console.WriteLine(item.ToString());
+
+            if (callback != null)
+            {
+                callback();
+            }
         }
     }
 }
